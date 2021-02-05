7d01da25032e224b373f2d62e020850d064e4a8e
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-05 17:04:58 +0900

    devise install

--- db/migrate/20210205020551_devise_create_users.rb
+++ db/migrate/20210205020551_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -11,7 +12,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name,         null: false
       t.string :last_name_kana,     null: false
       t.string :first_name_kana,    null: false
-      t.date   :birth_data,         null: false
+      t.date   :birth_date,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
