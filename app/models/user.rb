afb87d7ff5b9819a119566d337b850090d20a1cd
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-05 19:30:19 +0900

    modelの省略

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,22 +7,16 @@ class User < ApplicationRecord
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-          validates :first_name
-          validates :last_name
-         end
+        with_options presence: true do
 
-         with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
-           validates :first_name_kana
-           validates :last_name_kana
-         end
+         validates :nickname
+         validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+         validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+         validates :last_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' }
+         validates :first_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' }
+         validates :birth_date
+        end
 
-         validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :birth_date, presence: true
 
          has_many :items
          has_many :orders
