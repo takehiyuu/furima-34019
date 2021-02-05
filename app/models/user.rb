bd1ddceb924b6f174292179c3c8dbfa58e1ddca4
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-05 18:12:44 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,29 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+          validates :first_name
+          validates :last_name
+         end
+
+         with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
+           validates :first_name_kana
+           validates :last_name_kana
+         end
+
+         validates :nickname, presence: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :last_name_kana, presence: true
+         validates :first_name_kana, presence: true
+         validates :birth_date, presence: true
+
+         has_many :items
+         has_many :orders
+
+
 end
