bcb8f5c3d4895c686855c26fe906d4252cd017de
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-08 18:16:47 +0900

    itemsモデル編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+     has_one :order
+     belongs_to :user
+     has_one_attached :image
+
+     PRICE_REGEX = /\A[0-9]+\z/.freeze
+     validates_format_of :price, with: PRICE_REGEX, message: '販売価格には半角数字のみ使用してください'
+
+     with_options presence: true do
+
+          validates :name
+          validates :info
+          validates :category_id
+          validates :image
+          validates :sales_status_id
+          validates :shipping_fee_status_id
+          validates :prefecture_id
+          validates :scheduled_delivery_id
+          validates :price, length: {minimum: 3, maximum:8}
+
+     end
 end
