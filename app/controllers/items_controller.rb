5c3fcf6ae3f7aa0089fb8713cc33ec049f3d9712
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-09 11:42:50 +0900

    items controller、ルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
      def index
+          @items = Item.all
+     end
+
+     def new
+          @item = Item.new
+     end
+
+     def create
+          item = Item.create(item_params)
+     end
+
+     private
+
+     def item_params
+          params.require(:item).permit(
+               :name, :category_id, :image, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
      end
 end
