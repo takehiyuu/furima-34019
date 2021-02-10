a1ffab19105d4ba7b349586f6bd8c285e19bc08e
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-10 18:32:14 +0900

    item paramsエラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,11 +10,11 @@ class ItemsController < ApplicationController
      end
 
      def create
-          item = Item.create(item_params)
-          if item.save
+          @item = Item.new(item_params)
+          if @item.save
                redirect_to root_path
           else
-               render :new
+               render 'new'
           end
      end
 
