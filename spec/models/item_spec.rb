d6c27cd05fea3e98be42d4005371ce37f608e422
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-10 19:22:40 +0900

    テストコード追記、修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,42 +10,42 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Image can't be blank")
     end
-    it "商品名が必須であること" do
+    it "商品名が必須であること,idが1だと登録できない" do
       @item.name = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Name can't be blank")
     end
-    it "商品の説明が必須であること" do
+    it "商品の説明が必須であること,idが1だと登録できない" do
       @item.info = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Info can't be blank")
     end
-    it "カテゴリーの情報が必須であること" do
+    it "カテゴリーの情報が必須であること,idが1だと登録できない" do
       @item.category_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
     end
-    it "商品の状態についての情報が必須であること" do
+    it "商品の状態についての情報が必須であること,idが1だと登録できない" do
       @item.sales_status_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Sales status can't be blank", "Sales status is not a number")
     end
-    it "配送料の負担についての情報が必須であること" do
+    it "配送料の負担についての情報が必須であること,idが1だと登録できない" do
       @item.shipping_fee_status_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping fee status can't be blank", "Shipping fee status is not a number")
     end
-    it "発送元の地域についての情報が必須であること" do
+    it "発送元の地域についての情報が必須であること,idが1だと登録できない" do
       @item.prefecture_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture can't be blank")
     end
-    it "発送までの日数についての情報が必須であること" do
+    it "発送までの日数についての情報が必須であること,idが1だと登録できない" do
       @item.scheduled_delivery_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank", "Scheduled delivery is not a number")
     end
-    it "販売価格についての情報が必須であること" do
+    it "販売価格についての情報が必須であること,idが1だと登録できない" do
       @item.price = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank")
@@ -59,6 +59,16 @@ RSpec.describe Item, type: :model do
       @item.price = "１０００"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price 販売価格には半角数字のみ使用してください")
+    end
+    it "半角英語だけでは登録できないこと" do
+      @item.price = "take"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price 販売価格には半角数字のみ使用してください")
+    end
+    it "販売価格が10,000,000以上では登録できないこと" do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price 300以上9999999以下で入力して下さい")
     end
   end
 end
