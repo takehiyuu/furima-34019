e0e7f417cd713c3aa40e5af0cc18dfb0699f9803
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-10 09:53:40 +0900

    itemテーブルテストコードexample

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "商品出品機能について" do
+    it "ログイン状態のユーザーのみ、商品出品ページへ遷移できること" do
+    end
+    it "ログアウト状態のユーザーは、商品出品ページへ遷移しようとすると、ログインページへ遷移すること" do
+    end
+    it "商品画像を1枚つけることが必須であること" do
+    end
+    it "商品名が必須であること" do
+    end
+    it "商品の説明が必須であること" do
+    end
+    it "カテゴリーの情報が必須であること" do
+    end
+    it "商品の状態についての情報が必須であること" do
+    end
+    it "配送料の負担についての情報が必須であること" do
+    end
+    it "発送元の地域についての情報が必須であること" do
+    end
+    it "発送までの日数についての情報が必須であること" do
+    end
+    it "販売価格についての情報が必須であること" do
+    end
+    it "販売価格は、¥300~¥9,999,999の間のみ保存可能であること" do
+    end
+    it "販売価格は半角数字のみ保存可能であること" do
+    end
+  end
 end
