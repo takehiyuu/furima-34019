e988759f21a71f7974cf60389a7301e292d4b602
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-11 17:01:54 +0900

    重複する変数定義文をまとめ更新に成功すると詳細表示ページに遷移するように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
      before_action :authenticate_user!, except: :index
+     before_action :set_item, only: [:show, :edit, :update]
      def index
           @items = Item.includes(:user).order("created_at DESC")
      end
@@ -18,20 +19,17 @@ class ItemsController < ApplicationController
      end
 
      def show
-          @item = Item.find(params[:id])
      end
 
      def edit
-          @item = Item.find(params[:id])
           unless user_signed_in? && current_user.id == @item.user_id
                redirect_to action: :index
           end
      end
 
      def update
-          @item = Item.find(params[:id])
           if @item.update(item_params)
-               redirect_to root_path
+               redirect_to item_path(@item.id)
           else
                render :edit
           end
@@ -43,5 +41,9 @@ class ItemsController < ApplicationController
      def item_params
           params.require(:item).permit(
                :name, :category_id, :info, :image, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+     end
+
+     def set_item
+          @item = Item.find(params[:id])
      end
 end
