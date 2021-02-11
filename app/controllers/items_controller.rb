81c1e6c401558cb6e75765f6bf390b2e4c6279bf
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-11 19:30:51 +0900

    重複した記述を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_index, only: [:edit, :update]
+  before_action :move_index, only: [:edit, :update, :destroy]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -34,7 +34,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user_id
     @item.destroy
     redirect_to root_path
     end
