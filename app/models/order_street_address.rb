e0dacfe1a01705494da45455ad111c82c22952bb
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-14 20:32:42 +0900

    記述ミス修正

--- app/models/order_street_address.rb
+++ app/models/order_street_address.rb
@@ -1,12 +1,12 @@
 class OrderStreetAddress
      include ActiveModel::Model
-     attr_accessor :postal_code, :city, :addresses, :phone_number, :prefecture_id, :user_id, :item_id, :token, :building,
+     attr_accessor :postal_code, :city, :addresses, :phone_number, :prefecture_id, :user_id, :item_id, :token, :building
 
      with_options presence: true do
-          validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "郵便番号を正しく入力してください"}
+          validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "郵便番号を正しく入力してください" }
           validates :city
           validates :addresses
-          validates :phone_number, format: {with: /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/, message: "携帯電話の番号を正しく入力してください"}
+          validates :phone_number, format: { with: /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/, message: "携帯電話の番号を正しく入力してください" }
           validates :prefecture_id, numericality: { other_than: 1 }
           validates :item_id
           validates :user_id
